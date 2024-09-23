dd3af4eb41ccacbaa5a6bcb4e53261f048192054
Creeper-MZ <91367752+Creeper-MZ@users.noreply.github.com>
2024-09-23 13:30:10 -0400

    Add file

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+WD14MODEL = "SmilingWolf/wd-eva02-large-tagger-v3"
+AESTHETIC = "shadowlilac/aesthetic-shadow-v2"
+RAW_DATA_DIR = os.path.join( "data", "raw_images")
+PROCESSED_DATA_DIR = os.path.join("data", "processed_images")
